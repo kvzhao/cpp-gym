37a7c23ed04f79a41364528381add7f804450bec
kevin.zhao <kevin.zhao@ui.com>
2023-11-05 16:58:32 +0800

    update: q1.1 opt

--- 1_1_IsUnique.cpp
+++ 1_1_IsUnique.cpp
@@ -1,31 +1,42 @@
 
 #include "gtest/gtest.h"
-#include <set>
+#include <iostream>
 #include <string>
 
-// TODO: The algo requires pure array
 bool IsUnique(const std::string &str) {
 
-  std::set<char> hash;
+  // ascii only has 128
+  if (str.length() > 128)
+    return false;
 
-  for (auto c : str) {
-    if (hash.count(c)) {
+  // space optimization
+  int hash = 0;
+
+  for (char c : str) {
+
+    int val = c - 'a';
+
+    if ((hash & (1 << val))) {
       return false;
     }
-    hash.insert(c);
+
+    hash |= (1 << val);
   }
 
   return true;
 }
 
-TEST(TEST_REPEATED_CHARS, IsUnique) { 
-    ASSERT_FALSE(IsUnique("aaa"));
-    ASSERT_FALSE(IsUnique("acdbea"));
+TEST(TEST_REPEATED_CHARS, IsUnique) {
+  ASSERT_FALSE(IsUnique("aaa"));
+  ASSERT_FALSE(IsUnique("00"));
+  ASSERT_FALSE(IsUnique("acdbea"));
+  ASSERT_FALSE(IsUnique("avzxcjthl;q5jnfhv;"));
 }
 
-TEST(TEST_UNIQUE_CHARS, IsUnique) { 
-    ASSERT_TRUE(IsUnique("abd")); 
-    ASSERT_TRUE(IsUnique("zab")); 
+TEST(TEST_UNIQUE_CHARS, IsUnique) {
+  ASSERT_TRUE(IsUnique("abd"));
+  ASSERT_TRUE(IsUnique("zab"));
+  ASSERT_TRUE(IsUnique("1234567890-=+`~"));
 }
 
 int main(int argc, char **argv) {
